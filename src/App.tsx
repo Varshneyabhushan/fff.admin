f20db58af08d0feeab01ad0fbbe4309f4c613434
varshneyabhushan <me@bhushan.fun>
2023-03-25 03:09:40 +0530

    clear app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div>
-      here is the app...
+      <Header/>
     </div>
   );
 }
