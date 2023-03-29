c1e8d0b7cc506d0d7aaa8ddb12285e3077aefecd
varshneyabhushan <me@bhushan.fun>
2023-03-29 15:02:32 +0530

    added imagePage

--- src/AlbumPage/index.tsx
+++ src/AlbumPage/index.tsx
@@ -1,8 +1,39 @@
+import { useLocation } from "react-router-dom"
+import Album from "../services/Db/models/album"
+import { Model } from "../services/Db/models/model"
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import { getImageUrl } from "../utils/models/getThumbnail";
+
 
 export default function AlbumPage() {
+
+    const location = useLocation()
+    if (!location.state.model || !location.state.album) {
+        //implement loading
+        return (
+            <div>loading...</div>
+        )
+    }
+
+    const model = location.state.model as Model
+    const album = location.state.album as Album
+
     return (
         <div>
-            this is the album page
+            {model._id}
+            {album.name}
+            <ImageList sx={{ width: "100%", height: "maxHeight" }} cols={5} rowHeight={200}>
+                {album.images?.map((image) => (
+                    <ImageListItem key={image.id}>
+                        <img
+                            src={getImageUrl(image.url)}
+                            alt={image.id}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                )) ?? ""}
+            </ImageList>
         </div>
     )
 }
