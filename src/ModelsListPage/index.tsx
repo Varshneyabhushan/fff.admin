14e314d618fe095dac2fc88e0d061e068fa62b75
varshneyabhushan <me@bhushan.fun>
2023-03-30 03:15:42 +0530

    create model added

--- src/ModelsListPage/index.tsx
+++ src/ModelsListPage/index.tsx
@@ -9,7 +9,10 @@ import Resource from "../utils/resource/Resource"
 import toResource from "../utils/resource/toResource"
 import ModelPagination from "./Pagination"
 import ModelList from "./ModelList"
-
+import { Button } from "@mui/material"
+import "./modelList.scss"
+import { useNavigate } from "react-router-dom"
+import { getHeaderState } from "../ModelPage/modelLink"
 
 const dbService = new DbService(config.dbAPIUrl)
 const ModelsPerPage = 20
@@ -33,6 +36,7 @@ export function ModelListPage() {
     return (
         <Fragment>
             <ErrorBoundary fallback={"error while loading pagination"}>
+                <ModelActions/>
                 <ModelPagination pageChange={pageChange} totalPages={totalPages} totalModels={totalModels} />
             </ErrorBoundary>
             <ErrorBoundary fallback={"error while loading models"}>
@@ -41,5 +45,33 @@ export function ModelListPage() {
                 </Suspense>
             </ErrorBoundary>
         </Fragment>
+    )
+}
+
+function ModelActions() {
+
+    const navigate = useNavigate()
+
+    function createNewModel() {
+        let addingModel = { name : "hot lady"} as Model
+        dbService.addModel(addingModel)
+            .then(modelId => {
+                addingModel._id = modelId
+
+                let state = {
+                    header : getHeaderState(addingModel),
+                    model : addingModel,
+                }
+
+                alert("model created : " + modelId)
+                navigate('/models/' + modelId, { state, replace : true })
+            })
+            .catch(e => alert("error while creating model : " + e?.message ?? ""))
+    }
+
+    return (
+        <div className="modelActions">
+            <Button onClick={createNewModel}>new model </Button>
+        </div>
     )
 }
