c694d00416a5c085eb0fb64923a3e6e36ac244e9
varshneyabhushan <me@bhushan.fun>
2023-04-05 06:03:02 +0530

    invalid imports

--- src/AlbumListPage/AlbumActions/importFromURL.ts
+++ src/AlbumListPage/AlbumActions/importFromURL.ts
@@ -1,13 +1,9 @@
 import config from "../../config";
 import addAlbumIfNotExist from "../../ModelsListPage/ModelActions/addModelFromURL/addAlbumIfNotExist";
-import createAlbumIfNotExist from "../../ModelsListPage/ModelActions/addModelFromURL/addAlbumIfNotExist/createAlbumIfNotExist";
 import makeAlbumMapByLink from "../../ModelsListPage/ModelActions/addModelFromURL/addAlbumIfNotExist/makeAlbumMapByLink";
-import saveNewImages from "../../ModelsListPage/ModelActions/addModelFromURL/addAlbumIfNotExist/saveNewImages";
 import makeEnsureSiteAlias from "../../ModelsListPage/ModelActions/addModelFromURL/makeEnsureSiteAlias";
 import DbService from "../../services/Db";
-import Album from "../../services/Db/models/album";
 import { Model } from "../../services/Db/models/model";
-import ScrapperService from "../../services/Scrapper";
 import { AlbumSuggestion, ScrappedModel } from "../../services/Scrapper/models";
 
 export default async function importFromURL(model : Model, url : string) {
