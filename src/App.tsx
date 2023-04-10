d62419da680cbd14bdb20fe948be0e366909b16a
varshneyabhushan <me@bhushan.fun>
2023-04-11 04:57:20 +0530

    added suspense for modelListPage

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,13 @@ function App() {
       <Routes>
         <Route path='/models' element={<Header />}>
           <Route path='/models'>
-            <Route index element={<ModelListPage />} />
+            <Route index element={<ModelListPageLoader />} />
             <Route path=':modelId'>
-              <Route index element={<ModelPageLoader/>}/>
+              <Route index element={<ModelPageLoader />} />
               <Route path='albums'>
-                <Route index element={<AlbumListPage/>} />
-                <Route path=':albumId' element={<AlbumPage/>}/>
-              </Route>  
+                <Route index element={<AlbumListPage />} />
+                <Route path=':albumId' element={<AlbumPage />} />
+              </Route>
             </Route>
           </Route>
         </Route>
@@ -35,7 +35,16 @@ function ModelPageLoader() {
   return (
     <ErrorBoundary fallback={"error while loading model page"}>
       <Suspense fallback={"loading model page"}>
-        <ModelPage/>
+        <ModelPage />
+      </Suspense>
+    </ErrorBoundary>
+  )
+}
+function ModelListPageLoader() {
+  return (
+    <ErrorBoundary fallback={"error while loading modellist page"}>
+      <Suspense fallback={"loading modellist page"}>
+        <ModelListPage />
       </Suspense>
     </ErrorBoundary>
   )
