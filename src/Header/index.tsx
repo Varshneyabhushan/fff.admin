bcb88674f93275010a5e1fc6d084b738508aea4b
varshneyabhushan <me@bhushan.fun>
2023-04-21 14:14:56 +0530

    change header logic

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -3,62 +3,88 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Button, useTheme } from "@mui/material"
+import { IconButton, useTheme } from "@mui/material"
 import QueueService from '../services/Queue';
 import config from '../config';
-import { Suspense, useState } from 'react';
-import Resource from '../utils/resource/Resource';
-import toResource from '../utils/resource/toResource';
-import ErrorBoundary from '../utils/resource/ErrorBoundary';
+import { useCallback, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Refresh } from '@mui/icons-material';
 
 const queueService = new QueueService(config.queueAPIUrl)
-const status = toResource(queueService.getTotalItems())
 
 export default function Header() {
 
-
-  const [resource, setResource] = useState<Resource<number>>(status)
-  function refreshStatus() {
-    setTimeout(() => {
-      setResource(toResource(queueService.getTotalItems()))
-    },
-    0)
-  }
-
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{ justifyContent : "end" }}>
-          <div style={{ display : "flex" }}>
-          <ErrorBoundary fallback={"status loading failed"}>
-            <QueueStatus resource={resource}/>
-          </ErrorBoundary>
-          <Button variant='contained' onClick={refreshStatus} > status </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Box>
-    <Outlet />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar sx={{ justifyContent: "end" }}>
+            <QueueStatus />
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Outlet />
     </>
   );
 }
 
-interface QueueStatusProps {
-  resource: Resource<number>;
-}
+//in seconds
+const refreshTime = 5000
 
-function QueueStatus({ resource }: QueueStatusProps) {
+function QueueStatus() {
 
   const theme = useTheme()
-  const result = resource.read()
+  const [totalItems, setTotalItems] = useState(0)
+  const [error, setError] = useState<string>()
+  const [loading, setLoading] = useState(true)
+
+  const refreshStatus = useCallback(() => {
+    queueService.getTotalItems()
+      .then(result => setTotalItems(result))
+      .catch(e => setError(e.message))
+      .finally(() => setLoading(false))
+  },
+  [setTotalItems, setError, setLoading]
+  )
+
+
+  useEffect(() => {
+    refreshStatus()
+    let timeout = setTimeout(() => {
+      refreshStatus()
+    }, refreshTime);
+
+    return () => clearTimeout(timeout)
+  },
+    [refreshStatus])
+
 
   return (
-    <Suspense fallback={"loading..."}>
-      <Typography variant='h6' sx={{ marginRight: theme.spacing(2) }}>
-        {result}
-      </Typography>
-    </Suspense>
+    <div style={{ display: "flex" }}>
+      {
+        loading ? "loading" :
+          (
+            <>
+              {
+                (error && error.length !== 0) ? error :
+                  (
+                    <>
+                      <Typography variant='h6' sx={{ marginRight: theme.spacing(2) }}>
+                        {totalItems}
+                      </Typography>
+                      <IconButton onClick={refreshStatus} color='inherit'>
+                        <Refresh />
+                      </IconButton>
+                    </>
+                  )
+              }
+            </>
+          )
+
+      }
+
+    </div>
+
+
   )
 }
